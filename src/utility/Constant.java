74d854b27000c91b1a9d4f5972d5dc7436513bc0
iainstorrie <iain.storrie.ext@siemens.com>
2017-06-08 14:59:30 +0100

    Updated Constant.java and removing old screenshots

--- src/utility/Constant.java
+++ src/utility/Constant.java
@@ -25,21 +25,22 @@ package utility;
 **********************************************************************
 * Change Log:
 * 
-* Date:
-* Author: 
-* Details:
+* Date: 08/06/2017
+* Author: Iain Storrie
+* Details: Change URL value to Siemen's Mobility test environment URL,
+* update Path_TestData and Path_ScreenShot values
 *
 *********************************************************************/
 public class Constant {
 
     //public static final String URL = "sl-intranet-int.company-net.com";
-	public static final String URL = "www.bbc.co.uk/news";
+	public static final String URL = "https://siemens-og-bykrw4c6spfh7rcf452vdehd-sdg1-test.mbaas1.sdg.feedhenry.com/#/login";
     
-    public static final String Path_TestData = "C://Users//istorrie876_adm//workspace//DemoProjectFramework//src//testData//";
+    public static final String Path_TestData = "C://Users//z003td0k//My Documents//MobilityAutomation//src//testData//";
     		 
     public static final String File_TestData = "TestData.xlsx";
     
-    public static final String Path_ScreenShot = "C://Users//istorrie876_adm//workspace//DemoProjectFramework//test-output//screenshots//";
+    public static final String Path_ScreenShot = "C://Users//z003td0k//My Documents//MobilityAutomation//MobilityFramework//test-output//screenshots//";
    
 	public static final int Col_TestCaseName = 0;	
 
