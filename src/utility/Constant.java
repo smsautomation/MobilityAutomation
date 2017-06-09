da79457e8034dc4593bcc6e1711ecfd3241c04dd
z003td0k <z003td0k@MD1N0QGC.ad001.siemens.net>
2017-06-09 10:47:35 +0100

    Update Test Data and Screenshot locations in Constant

--- src/utility/Constant.java
+++ src/utility/Constant.java
@@ -25,7 +25,7 @@ package utility;
 **********************************************************************
 * Change Log:
 * 
-* Date: 08/06/2017
+* Date: 09/06/2017
 * Author: Iain Storrie
 * Details: Change URL value to Siemen's Mobility test environment URL,
 * update Path_TestData and Path_ScreenShot values
@@ -33,14 +33,13 @@ package utility;
 *********************************************************************/
 public class Constant {
 
-    //public static final String URL = "sl-intranet-int.company-net.com";
-	public static final String URL = "https://siemens-og-bykrw4c6spfh7rcf452vdehd-sdg1-test.mbaas1.sdg.feedhenry.com/#/login";
+    public static final String URL = "https://siemens-og-bykrw4c6spfh7rcf452vdehd-sdg1-test.mbaas1.sdg.feedhenry.com/#/login";
     
-    public static final String Path_TestData = "C://Users//z003td0k//My Documents//MobilityAutomation//src//testData//";
+    public static final String Path_TestData = "C://MobilityAutomation//src//testData//";
     		 
     public static final String File_TestData = "TestData.xlsx";
     
-    public static final String Path_ScreenShot = "C://Users//z003td0k//My Documents//MobilityAutomation//MobilityFramework//test-output//screenshots//";
+    public static final String Path_ScreenShot = "C://MobilityAutomation//MobilityFramework//test-output//screenshots//";
    
 	public static final int Col_TestCaseName = 0;	
 
